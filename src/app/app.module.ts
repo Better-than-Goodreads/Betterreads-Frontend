c13da87efe325df7d11c829a6790294d49846bdd
LuchoLorenzo1 <LuchoLorenzo1@users.noreply.github.com>
2024-10-18 09:41:11 -0300

    agrego una simple estructura de libro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import {
 } from "@angular/material/form-field";
 import { AppRoutingModule } from "./app-routing.module";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatNativeDateModule } from "@angular/material/core";
+import { MatNativeDateModule, MatOption } from "@angular/material/core";
 import { MatInputModule } from "@angular/material/input";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatButtonModule } from "@angular/material/button";
@@ -26,11 +26,11 @@ import { MatDividerModule } from "@angular/material/divider";
 import { MatStepperModule } from "@angular/material/stepper";
 import { RegistrarseComponent } from "./pantallas/registrarse/registrarse.component";
 import { MatCheckboxModule } from "@angular/material/checkbox";
-
 import { HttpClientModule } from '@angular/common/http';
 import { environment } from '../environments/environment';
-
 import { UsuariosService } from './services/usuarios.service';
+import { MatSelect } from "@angular/material/select";
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle } from "@angular/material/card";
 
 registerLocaleData(localeEsAr, "es-Ar");
 
@@ -63,6 +63,14 @@ registerLocaleData(localeEsAr, "es-Ar");
     MatDividerModule,
     MatStepperModule,
     MatCheckboxModule,
+    MatOption,
+    MatSelect,
+	MatCard,
+	MatCardHeader,
+	MatCardSubtitle,
+	MatCardTitle,
+	MatCardContent,
+	MatCardActions,
   ],
   providers: [
     { provide: LOCALE_ID, useValue: "es-Ar" },
