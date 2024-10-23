138ee2d232fa9cedb7f38939fe9ddca8f612d76c
LuchoLorenzo1 <LuchoLorenzo1@users.noreply.github.com>
2024-10-23 09:39:00 -0300

    agrego llamadas a la api para puntuar en servicio y componente libro

--- src/app/componentes/libro/libro.component.ts
+++ src/app/componentes/libro/libro.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Libro } from '../../entidades/Libro';
+import { BookService } from '../../services/servicio-libros.service';
 
 @Component({
   selector: 'app-libro',
@@ -9,7 +10,16 @@ import { Libro } from '../../entidades/Libro';
 export class LibroComponent {
 	@Input() book: Libro = new Libro();
 
+	constructor(private bookService: BookService) { }
+	
+	ngOnInit() {
+		this.bookService.getRating(this.book.id).subscribe((data: any) => {
+			this.book.stars = data.rating;
+		});
+	}
+
 	puntuar(i: number) {
+		this.bookService.rateBook(this.book.id, i).subscribe();
 		this.book.stars = i;
 	}
 }
--- src/app/services/servicio-libros.service.ts
+++ src/app/services/servicio-libros.service.ts
@@ -4,20 +4,34 @@ import { Observable } from 'rxjs';
 import { Libro } from '../entidades/Libro';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class BookService {
-  private apiUrl = 'http://localhost:8080/books';  
+	private apiUrl = 'http://localhost:8080/books';
 
-  constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient) { }
 
-  getBooks(): Observable<Libro[]> {
-    return this.http.get<Libro[]>(this.apiUrl);
-  }
+	getToken() {
+		return sessionStorage.getItem('token');
+	}
 
-  postBook(b: any): Observable<any> {
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    
-    return this.http.post(this.apiUrl, JSON.stringify(b), { headers });
-  }
+	getBooks(): Observable<Libro[]> {
+		const headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.getToken() });
+		return this.http.get<Libro[]>(this.apiUrl, { headers });
+	}
+
+	postBook(b: any): Observable<any> {
+		const headers = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + this.getToken() });
+		return this.http.post(this.apiUrl, JSON.stringify(b), { headers });
+	}
+
+	rateBook(bookId: string, rating: number): Observable<any> {
+		const headers = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + this.getToken() });
+		return this.http.post(this.apiUrl + `/${bookId}/rating`, JSON.stringify({ rating: rating }), { headers });
+	}
+
+	getRating(bookId: string): Observable<any> {
+		const headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.getToken() });
+		return this.http.post(this.apiUrl + `/${bookId}/rating`, { headers });
+	}
 }
