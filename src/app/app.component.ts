b52f4c702c90ccff7b084c60069488694f722196
antopell <antopell29@gmail.com>
2024-10-29 17:09:54 -0300

    Arreglo altura de menu + agrego link a perfil desde barra superior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,12 +16,21 @@ export class AppComponent {
         const currentUrl = event.url;
         // Check the URL to determine if the sidenav should be visible
         this.showSidenav = !this.muestraSidenav(currentUrl);
-        this.usuarioActual = sessionStorage.getItem('username')?? '';
-        const id = sessionStorage.getItem('id');
-        this.urlFotoPerfil = id ? `http://localhost:8080/users/${id}/picture` : '';
+        this.usuarioActual = sessionStorage.getItem('username')?? 'Log In';
+        this.id = sessionStorage.getItem('id')?? '';
+        this.urlFotoPerfil = this.id ? `http://localhost:8080/users/${this.id}/picture` : './default-profile.png';
       }
     });
 
+  }
+
+  id = '';
+  linkZonaUsuario() {
+    if (this.id) {
+      this.router.navigate(['/user', this.id]);
+    } else {
+      this.router.navigate(['/log-in']);
+    }
   }
 
   usuarioActual = '';
